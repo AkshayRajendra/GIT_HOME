d066745deba14d1eef8e52ed42cf6893f0861515
akshay.addi@gmail.com <akshay.addi@gmail.com>
2018-03-29 16:20:31 +0530

    hellowrd

--- test1.java
+++ test1.java
@@ -1,8 +1,10 @@
 public class Helloworld
 {
-	public static void main(strings[] args)
+	public static void main(String[] args)
 	{
 		for(int i=0;i<=10;i++)
-		System.out.println("Hello World"+i)
+		{
+		System.out.println("Hello World"+i);
+		}
 	}
 }
